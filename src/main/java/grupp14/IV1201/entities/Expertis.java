da0385ed325b3510deac2d77e6526a3b47967b05
mavencast <mavencast@gmail.com>
2016-03-04 10:53:24 +0100

    Added Expertis entity

---
+++ src/main/java/grupp14/IV1201/entities/Expertis.java
@@ -0,0 +1,60 @@
+/*
+ * Course project - IV1201 Design of Global Applications
+ * Royal Institute of Technology
+ * 2015 (c) Kim Hammar Alexander Lundh Marcel Mattsson
+ */
+package grupp14.IV1201.entities;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author marcelmattsson
+ */
+@Entity
+public class Expertis implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private BigInteger id;
+    private String expertis;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Expertis)) {
+            return false;
+        }
+        Expertis other = (Expertis) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "grupp14.IV1201.entities.Expertis[ id=" + id + " ]";
+    }
+    
+}
